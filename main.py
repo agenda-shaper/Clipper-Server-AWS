4698cf4768756f39f3927271ca82ba2c4415f315
yamete <eimantaslimba7+DevastMod@gmail.com>
2023-04-25 22:19:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,31 @@ import websockets
 import json
 
 
-async def handle_youtube(message):
+async def handle_youtube_whisper(message):
     # Process the YouTube message here
     print(f"Processing YouTube message: {message}")
+    model_inputs = {
+        "url": message["url"],
+        "start_time": message["start_time"],
+        "end_time": message["end_time"],
+        "kwargs": {"beam_size": 5, "temperature": [0, 0.2, 0.4, 0.6, 0.8, 1]},
+    }
+    print(model_inputs)
+
+    api_key = "c9a5be2f-b57b-4419-8599-d89f4b24bfa3"
+
+    model_key_medium = "4a4f2fe0-017a-46bf-b8dc-5c1c6b8458a9"
+
+    # Run the model
+    response = banana.run(api_key, model_key_medium, model_inputs)
+
+    # Convert the JSON response to a Python dictionary
+    result_dict = json.loads(response.text)
+
+    # Convert the dictionary to a JSON-formatted string
+    result_json = json.dumps(result_dict)
+
+    return result_json
 
 
 async def handle_websocket(websocket, path):
@@ -14,8 +36,9 @@ async def handle_websocket(websocket, path):
         print(f"Received message: {message}")
         try:
             data = json.loads(message)
-            if data.get("type") == "youtube":
-                await handle_youtube(data)
+            if data["type"] == "youtube_transcribe":
+                result_json = await handle_youtube_whisper(data)
+                await websocket.send(result_json)
             else:
                 response = f"You sent: {message}"
                 await websocket.send(response)
