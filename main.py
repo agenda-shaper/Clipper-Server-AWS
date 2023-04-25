1ecac63bb6bf1a7acb8689accea5e170ab2219de
yamete <eimantaslimba7+DevastMod@gmail.com>
2023-04-25 20:39:03 +0300

    main

---
+++ main.py
@@ -0,0 +1,19 @@
+import asyncio
+import websockets
+import banana_dev as banana
+
+
+async def handle_websocket(websocket, path):
+    async for message in websocket:
+        print(f"Received message: {message}")
+        response = f"You sent: {message}"
+        await websocket.send(response)
+
+
+async def main():
+    async with websockets.serve(handle_websocket, "localhost", 80):
+        print("WebSocket server started")
+        await asyncio.Future()  # Keep the event loop running
+
+
+asyncio.run(main())
