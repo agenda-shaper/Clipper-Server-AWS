7effabb5872b1ed0d039c5f4b93c5f20bbb87037
yamete <eimantaslimba7+DevastMod@gmail.com>
2023-04-29 19:04:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,7 @@ import websockets
 import json
 
 
-async def handle_youtube_whisper(message):
-    # Process the YouTube message here
-    print(f"Processing YouTube message: {message}")
-    model_inputs = {
-        "url": message["url"],
-        "start_time": message["start_time"],
-        "end_time": message["end_time"],
-        "kwargs": {"beam_size": 5, "temperature": [0, 0.2, 0.4, 0.6, 0.8, 1]},
-    }
-    # print(model_inputs)
-
+async def handle_banana_dev(model_inputs):
     api_key = "c9a5be2f-b57b-4419-8599-d89f4b24bfa3"
 
     model_key_medium = "4a4f2fe0-017a-46bf-b8dc-5c1c6b8458a9"
@@ -33,12 +23,8 @@ async def handle_websocket(websocket, path):
         print(f"Received message: {message}")
         try:
             data = json.loads(message)
-            if data["type"] == "youtube_transcribe":
-                result_json = await handle_youtube_whisper(data)
-                await websocket.send(result_json)
-            else:
-                response = f"You sent: {message}"
-                await websocket.send(response)
+            result_json = await handle_banana_dev(data)
+            await websocket.send(result_json)
         except json.JSONDecodeError:
             print("Invalid JSON received")
             await websocket.send("Invalid JSON format")
